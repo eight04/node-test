b41710a5891c3e1e14119c5094c54c2b9a60a5e1
eight04 <eight04@gmail.com>
2023-02-10 03:05:42 +0800

    Remove unused files

--- index.js
+++
@@ -1,6 +0,0 @@
-const {VM} = require("vm2");
-
-const vm = new VM({
-  sandbox: {input: "foo"}
-});
-console.log(vm.run("input"));
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export function foo(fn: (...args: any[]) => any) {
-  return fn(...[1, 2, 3]);
-}
